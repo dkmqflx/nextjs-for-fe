151bfb89f0e269f22ccdcec10012cc5ff29d629b
dkmqflx <dkmqflx@gmail.com>
2026-02-03 22:20:13 +0900

    23. [실습] 첫 NestJS 프로젝트 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 모듈이란 컨트롤러, 서비스, 모듈 등을 모아둔 파일
 @Module({
   imports: [],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() // 어디서든 인젝션을 받을 수 있어야 하므로 injectable 이라는 데코레이터 있음
 export class AppService {
   getHello(): string {
     return 'Hello World!';
